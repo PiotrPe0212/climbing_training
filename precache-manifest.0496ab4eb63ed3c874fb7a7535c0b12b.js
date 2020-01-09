a3ebe57def6f6a539859f82df1a45a5175c45954
Piotr_Pietraszek <pan.pietraszek@gmail.com>
2020-01-09 19:56:53 +0100

    Updates

---
+++ precache-manifest.0496ab4eb63ed3c874fb7a7535c0b12b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "872bf0f78b98e068a0ddab26f42e98d7",
+    "url": "/climbing_training/index.html"
+  },
+  {
+    "revision": "476f55dd71f2925ed84b",
+    "url": "/climbing_training/static/css/main.07223b29.chunk.css"
+  },
+  {
+    "revision": "e3f707372141bd097ece",
+    "url": "/climbing_training/static/js/2.f9021394.chunk.js"
+  },
+  {
+    "revision": "c9b0142727a50cc03f66094cc900509f",
+    "url": "/climbing_training/static/js/2.f9021394.chunk.js.LICENSE"
+  },
+  {
+    "revision": "476f55dd71f2925ed84b",
+    "url": "/climbing_training/static/js/main.9f1a3994.chunk.js"
+  },
+  {
+    "revision": "14f54ce7d1ead0aaed3c",
+    "url": "/climbing_training/static/js/runtime-main.06576d16.js"
+  },
+  {
+    "revision": "094e225c340be25fc0b94bce7f4d33e8",
+    "url": "/climbing_training/static/media/_DSC8125.094e225c.jpg"
+  }
+]);
