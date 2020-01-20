75901b17008b6c320ebd2573085fe25bad702efa
Piotr_Pietraszek <pan.pietraszek@gmail.com>
2020-01-20 18:09:00 +0100

    update

---
+++ build/precache-manifest.d9844e486a7a12a446c17254c7cc8f7d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca5b1e8f86347c3c1bf14a35b9074e9b",
+    "url": "/climbing_training/index.html"
+  },
+  {
+    "revision": "3b067d4b0bcf242a2a2c",
+    "url": "/climbing_training/static/css/main.b599a6fd.chunk.css"
+  },
+  {
+    "revision": "fa05066a3f51586c25d7",
+    "url": "/climbing_training/static/js/2.18d2ac47.chunk.js"
+  },
+  {
+    "revision": "c9b0142727a50cc03f66094cc900509f",
+    "url": "/climbing_training/static/js/2.18d2ac47.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b067d4b0bcf242a2a2c",
+    "url": "/climbing_training/static/js/main.302f2185.chunk.js"
+  },
+  {
+    "revision": "14f54ce7d1ead0aaed3c",
+    "url": "/climbing_training/static/js/runtime-main.06576d16.js"
+  },
+  {
+    "revision": "beca89d5da25286a87d167884f2cd5e6",
+    "url": "/climbing_training/static/media/bgImg-2.beca89d5.jpg"
+  },
+  {
+    "revision": "0073b9e01fd199ebe4203e15afd1844d",
+    "url": "/climbing_training/static/media/climber.0073b9e0.png"
+  }
+]);
